d9ec22b5465fd991c52e9ee1aca7a3d0da7e0883
dok <aporwitz@gmail.com>
2026-01-18 20:51:27 -0700

    create Loco(motion) class

--- CircuitPython code/motor_dr.py
+++ CircuitPython code/motor_dr.py
@@ -3,12 +3,11 @@ DrnkenR0bot DC motor library for 2 motor locomotion.
 """
 import pwmio
 
-
 class Motor:
     def __init__(self, pin1, pin2, frequency=2000):
         self.IN1 = pwmio.PWMOut(pin1, frequency=frequency)
         self.IN2 = pwmio.PWMOut(pin2, frequency=frequency)
-        self.max_duty_cycle = 65536
+        self.max_duty_cycle = 65535
         self.min_duty_cycle = 0
 
     def stop(self):
@@ -22,4 +21,29 @@ class Motor:
     def backward(self, speed: float = 1.0):
         self.IN1.duty_cycle = 0
         self.IN2.duty_cycle = int(self.max_duty_cycle*speed)
+
+class Loco:
+    def __init__(self, left_motor: Motor, right_motor: Motor):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+
+    def stop(self):
+        self.left_motor.stop()
+        self.right_motor.stop()
+
+    def forward(self, speed: float = 1.0):
+        self.left_motor.forward(speed=speed)
+        self.right_motor.forward(speed=speed)
+
+    def backward(self, speed: float = 1.0):
+        self.left_motor.backward(speed=speed)
+        self.right_motor.backward(speed=speed)
+
+    def right_spin(self, speed: float = 0.25):
+        self.left_motor.forward(speed=speed)
+        self.right_motor.backward(speed=speed)
+
+    def left_spin(self, speed: float = 0.25):
+        self.left_motor.backward(speed=speed)
+        self.right_motor.forward(speed=speed)
 
