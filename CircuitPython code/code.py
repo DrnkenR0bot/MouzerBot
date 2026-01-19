976c5865e1b521328507d8109cde3fad60cd3984
dok <aporwitz@gmail.com>
2026-01-19 13:54:22 -0700

    add sonar code and basic test

--- CircuitPython code/code.py
+++ CircuitPython code/code.py
@@ -1,7 +1,19 @@
 import board
 import digitalio
+import adafruit_hcsr04
 from time import sleep
 from motor_dr import Motor, Loco
+
+# Initialize sonar
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.A3, echo_pin=board.A2)
+
+def distance(cm2xscale=1e-2):
+    """
+    Return sonar distance to object.
+    
+    :param cm2xscale: Use to scale from native cm to any units. Default is meters (1e-2).
+    """
+    return sonar.distance*cm2xscale
 
 # Initialize left and right motors
 left_motor = Motor(board.D2, board.D3)
@@ -16,8 +28,9 @@ SLP = digitalio.DigitalInOut(board.D6)
 SLP.switch_to_output()
 SLP.value = True
 
-# As a test, drive straight, reverse, turn, turn, and stop
+
 def drive_test(drive_speed: float, turn_speed: float):
+    print("Initiating drive test...")
     sleep(5)
 
     loco.forward(speed=drive_speed)
@@ -41,5 +54,17 @@ def drive_test(drive_speed: float, turn_speed: float):
     sleep(1)
     # we should be back where we started, depending on motor control accuracy
 
+def distance_test():
+    print("Initiating sonar test, KeyboardInterrupt to stop...")
+    sleep(3)
+    while True:
+        try:
+            print(distance())
+            sleep(0.5)
+        except KeyboardInterrupt:
+            break
+
+
 if __name__ == "__main__":
     drive_test(0.5, 0.25)
+    distance_test()
